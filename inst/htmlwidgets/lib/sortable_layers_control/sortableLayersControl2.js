1f06b206ab510a548ad3ab30dc53e19694d42457
timelyportfolio <kent.russell@timelyportfolio.com>
2016-08-17 16:24:14 -0500

    approach number 2 sort of "working" with svg; still buggy though

--- inst/htmlwidgets/lib/sortable_layers_control/sortableLayersControl2.js
+++ inst/htmlwidgets/lib/sortable_layers_control/sortableLayersControl2.js
@@ -4,10 +4,12 @@ LeafletWidget.methods.sortableLayersControl2 = function () {
 
   var layerList = map.currentLayersControl._overlaysList;
 
+  sortLayers(layerList);
+
   Sortable.create(layerList, {
     animation: 150,
     onUpdate: function(evt) {
-      sortLayers();
+      sortLayers(layerList);
     }
   });
 
@@ -15,8 +17,10 @@ LeafletWidget.methods.sortableLayersControl2 = function () {
     var layers = map.currentLayersControl._layers;
     var layer = {};
     for(var i in layers){
-      if(layers[i].name === layerName) layer = layers[i];
-      break;
+      if(layers[i].name === layerName){
+        layer = layers[i];
+        break;
+      }
     }
     return layer;
   }
@@ -31,9 +35,43 @@ LeafletWidget.methods.sortableLayersControl2 = function () {
     return layerNames;
   }
 
-  function sortLayers() {
+  function getElements(layerGroup) {
+    var els = $.map(
+      layerGroup.layer._layers,
+      function(layer){
+        return layer._container;
+      }
+    );
+
+    return els;
+  };
+
+  function sortLayers(layerList) {
+    // currently only supports svg overlays
+    //   easy enough though to add z-index to non-svg
+    //   but difficult because all svg elements reside in a single svg
+    //   so svg combined with non-svg where the non-svg ordered between members
+    //   of svg will not work unless we split our svg container
+    //   into multiple svgs and place them in separate divs
+    //   not impossible but a lot more intrusive
+
     var ids = getIds(layerList);
-    var layers = ids.map(getLayer);
+    var layers = ids.map(function(i,id){return getLayer(id);});
+
+    // Should the top layer be placed on top or the bottom placed on top?
+    //   currently bottom layer in layer control will be placed on top
+    //   to change just remove reverse below
+
+
+    //build from last to first so reverse
+    layers.toArray().reverse().map(function(layer){
+      var els = $(getElements(layer));
+      var svg = els.parent();
+      //remove the elements
+      els.detach();
+      //add them back to beginning of parent svg
+      svg.prepend(els);
+    });
   }
 
   L.DomEvent.disableClickPropagation(layerList);
